9d18855a8ca0fed287128aed7a9032c8ec36dd9d
lysyuk <lysyuk.y@gmail.com>
2022-01-18 18:29:36 +0300

    Create modal form

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,22 @@
-$(function () {
+'use strict';
+
+$(document).ready(function () {
+  // Modal
+  $('#modal').iziModal({
+    title: 'Оставьте заявку',
+    subtitle: 'Мы перезвоним вам в ближайшие 15 минут',
+    background: '#1c222a',
+    headerColor: '#1c222a',
+    icon: 'fas fa-thumbs-up',
+    iconColor: '#edb021',
+    width: 400,
+    padding: 20,
+  });
+
+  $(document).on('click', '.trigger', function (event) {
+    event.preventDefault();
+    $('#modal').iziModal('open');
+  });
 
   //E-mail Ajax Send
   //Documentation & Example: https://github.com/agragregra/uniMail
