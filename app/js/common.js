57be2980cdec88c340c4824f142d35599071125c
lysyuk <lysyuk.y@gmail.com>
2022-01-18 20:24:46 +0300

    Add thank you modal

--- app/js/common.js
+++ app/js/common.js
@@ -13,6 +13,20 @@ $(document).ready(function () {
     padding: 20,
   });
 
+  let modalThanks = $('#thank-you').iziModal({
+    title: 'Спасибо!',
+    subtitle: 'Ожидайте звонка в течении 15 минут',
+    background: '#1c222a',
+    headerColor: '#1c222a',
+    icon: 'fas fa-thumbs-up',
+    iconColor: '#edb021',
+    width: 400,
+    timeout: 5000,
+    timeoutProgressbar: true,
+    timeoutProgressbarColor: '#edb021',
+    pauseOnHover: true,
+  });
+
   $(document).on('click', '.trigger', function (event) {
     event.preventDefault();
     $('#modal').iziModal('open');
