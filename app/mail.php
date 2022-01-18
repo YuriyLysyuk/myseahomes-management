1bdb83653277854d74989489393af34e5c30eebf
lysyuk <lysyuk.y@gmail.com>
2022-01-18 20:25:12 +0300

    Set mail settings

--- app/mail.php
+++ app/mail.php
@@ -2,13 +2,16 @@
 
 $method = $_SERVER['REQUEST_METHOD'];
 
+$project_name = 'Маркетинг | Myseahomes';
+$admin_email  = 'myseahomes@yandex.com';
+$from_email = 'info@myseahomes.com';
+$form_subject = 'Новая заявка c myseahomes.com/boost/';
+
 //Script Foreach
 $c = true;
 if ( $method === 'POST' ) {
 
-	$project_name = trim($_POST["project_name"]);
-	$admin_email  = trim($_POST["admin_email"]);
-	$form_subject = trim($_POST["form_subject"]);
+	
 
 	foreach ( $_POST as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
@@ -21,10 +24,6 @@ if ( $method === 'POST' ) {
 	}
 }
 } else if ( $method === 'GET' ) {
-
-	$project_name = trim($_GET["project_name"]);
-	$admin_email  = trim($_GET["admin_email"]);
-	$form_subject = trim($_GET["form_subject"]);
 
 	foreach ( $_GET as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
@@ -46,7 +45,8 @@ function adopt($text) {
 
 $headers = "MIME-Version: 1.0" . PHP_EOL .
 "Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
+'From: '.adopt($project_name).' <'.$from_email.'>' . PHP_EOL .
 'Reply-To: '.$admin_email.'' . PHP_EOL;
 
-mail($admin_email, adopt($form_subject), $message, $headers );
+ mail($admin_email, adopt($form_subject), $message, $headers );
+
