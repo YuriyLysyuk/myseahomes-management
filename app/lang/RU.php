b40e78a4832ee946c98fda61cbd8dc881e880ade
lysyuk <lysyuk.y@gmail.com>
2022-01-23 21:12:00 +0300

    Add php-multi;ang RU lang example

---
+++ app/lang/RU.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Ru translate for php-multilang
+ * Copy into path/to/php-multilang/languages/
+ * 
+ * Add new ''=>'',
+ */
+
+$RU =  [
+  'test'=>'test',
+
+];
+
+/** Not Yet Translated **/
